411ff84ecb950ad8b6c8ff6b9631b32adf7b1fc8
Cleython Santos <135386524+CleythonSantos@users.noreply.github.com>
2025-09-25 21:25:48 -0300

    Delete Exercício - Sofia directory

--- Exercício - Sofia/main.py
+++
@@ -1,7 +0,0 @@
-from utils import ler_float 
-
-nota1 = ler_float("Digite a primeira nota: ")
-nota2 = ler_float("Digite a segunda nota: ")
-
-media = (nota1 + nota2) / 2
-print(f"A média das notas é: {media}")
--- Exercício - Sofia/utils.py
+++
@@ -1,8 +0,0 @@
-def ler_float(msg):
-
-    while True:
-        try:
-            valor = float(input(msg))
-            return valor
-        except ValueError:
-            print("Erro: Digite um número válido!")
